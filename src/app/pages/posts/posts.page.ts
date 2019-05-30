efdf142e702d9c32b5165b1bfa5ef830c0493f0e
“Alice_Costa” <alice@fundacaocefetminas.org.br>
2019-05-30 12:51:22 -0300

    Contextos do banco

--- src/app/pages/posts/posts.page.ts
+++ src/app/pages/posts/posts.page.ts
@@ -1,19 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { Http, Headers, RequestOptions } from '@angular/http';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.page.html',
   styleUrls: ['./posts.page.scss'],
 })
+
 export class PostsPage implements OnInit {
 
   sliderConfig = {
     autoHeight: true
   };
 
-  constructor() { }
+  contexts: any;
+
+  constructor(private storage: Storage, private http: Http) { }
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter() {
+		this.showContexts();
+	}
+
+  showContexts() {
+    this.storage.get('token').then(token => {
+      if (token) {
+        let data = { 'token': token };
+        let headers = new Headers(
+          {
+            'Content-Type': 'application/json'
+          });
+        let options = new RequestOptions({ headers: headers });
+        this.http.post('https://api.fundacaocefetminas.org.br/getPosts', data, options)
+          .toPromise()
+          .then((response) => {
+            if (response.json().success) {
+              this.contexts=response.json().contexts
+              console.log("success");    
+            }else{
+              console.log("fail");
+            }
+          })
+      }
+    })
   }
 
 }
