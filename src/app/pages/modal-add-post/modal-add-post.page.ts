b378f93fb4be04429e78429057054e2a6f33575c
“Alice_Costa” <alice@fundacaocefetminas.org.br>
2019-06-03 12:01:13 -0300

    Editando modal add post

--- src/app/pages/modal-add-post/modal-add-post.page.ts
+++ src/app/pages/modal-add-post/modal-add-post.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { FormGroup } from '@angular/forms';
+import { Storage } from '@ionic/storage';
+import { Http, Headers, RequestOptions } from '@angular/http';
 
 @Component({
   selector: 'app-modal-add-post',
@@ -8,9 +11,16 @@ import { ModalController } from '@ionic/angular';
 })
 export class ModalAddPostPage implements OnInit {
 
-  constructor(private modalCrtl: ModalController) { }
+  addPostForm: FormGroup;
+  contexts: any;
+
+  constructor(private modalCrtl: ModalController, private storage: Storage, private http: Http) { }
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter() {
+      this.showVisibleContexts();
   }
 
   closeModal() {
@@ -28,5 +38,32 @@ export class ModalAddPostPage implements OnInit {
       (<HTMLInputElement>document.getElementById('img_name')).value = (<HTMLInputElement>document.getElementById('img_name')).value + img_files[i].name + '\n';
     }
   }
+
+  showVisibleContexts() {
+    this.storage.get('token').then(token => {
+      if (token) {
+        let data = { 'token': token };
+        let headers = new Headers(
+          {
+            'Content-Type': 'application/json'
+          });
+        let options = new RequestOptions({ headers: headers });
+        this.http.post('https://api.fundacaocefetminas.org.br/getVisibleContexts', data, options)
+          .toPromise()
+          .then((response) => {
+            if (response.json().success) {
+              this.contexts = response.json().contexts;
+            } else {
+              console.log("Fail");
+            }
+          })
+      }
+    })
+  }
+
+  submitAddPost(){
+		let data ={};
+		console.log(data);
+	}
 
 }
